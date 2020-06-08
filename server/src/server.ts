61c12e2d88832d36e0c888ee691a27cb5815ae07
Henrique Pini <henriqueflopi@hotmail.com>
2020-06-07 21:37:25 -0300

    Add celebrate validation feature

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import path from 'path';
 import routes from './routes';
+import { errors } from 'celebrate';
 
 const app = express();
 
@@ -11,6 +12,8 @@ app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads'))); // Rota estatica, para devolver arquivos.
 
+app.use(errors());
+
 app.listen(3333, () => {
-  console.log('Server started on http://localhost:3333/');
+  console.log('Server started on http://192.168.0.109:3333/');
 });
